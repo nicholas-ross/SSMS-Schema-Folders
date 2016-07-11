6a1d280927d91059207b6110b83f364643969c75
Nicholas Ross <nicholas.ross@live.com.au>
2016-07-11 14:32:43 +1000

    Added detail attributes to the options.

--- SchemaFolderOptions.cs
+++ SchemaFolderOptions.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using System.ComponentModel;
 
 namespace SsmsSchemaFolders
 {
     public class SchemaFolderOptions : DialogPage
     {
+        [Category("Enabled")]
+        [DisplayName("Enabled")]
+        [Description("Group sql objects in Object Explorer (tables, views, etc.) into schema folders.")]
         public bool Enabled { get; set; } = true;
+
+        [Category("Folder Display Options")]
+        [DisplayName("Append Dot")]
+        [Description("Add a dot after the schema name on the folder label. ")]
         public bool AppendDot { get; set; } = true;
+
+        [Category("Folder Display Options")]
+        [DisplayName("Clone Parent Node")]
+        [Description("Add the right click and drag and drop functionality of the parent node to the schema folder node.")]
         public bool CloneParentNode { get; set; } = true;
+
+        [Category("Folder Display Options")]
+        [DisplayName("Use Object Icon")]
+        [Description("Use the icon of the first child node as the folder icon. If false then use the parent node (i.e. folder) icon.")]
         public bool UseObjectIcon { get; set; } = true;
     }
 }
