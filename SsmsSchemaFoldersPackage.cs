7f118e55bbf31e76331638306022303a0e07c925
Nicholas Ross <nicholas.ross@live.com.au>
2016-07-11 22:51:55 +1000

    Refactored code from ssms2012extender. Changed how tree view event handlers are attached. Made use of interfaces.

--- SsmsSchemaFoldersPackage.cs
+++ SsmsSchemaFoldersPackage.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.SqlServer.Management.SqlStudio.Explorer;
-using Microsoft.SqlServer.Management.UI.VSIntegration.ObjectExplorer;
+﻿using Microsoft.SqlServer.Management.UI.VSIntegration.ObjectExplorer;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
@@ -42,14 +41,8 @@ namespace SsmsSchemaFolders
 
         public SchemaFolderOptions Options { get; set; }
 
-        ObjectExplorerService _objExplorerService;
+        IObjectExplorerService _objExplorerService;
         ObjectExplorerExtender _objectExplorerExtender;
-        delegate void TrvEventAfterExpand(object obj, TreeViewEventArgs e);
-        delegate void TrvEventBeforeExpand(object obj, TreeViewCancelEventArgs e);
-
-        ContextService _cs;
-        TreeView _trv = null;
-        bool _attached = false;
 
         // Ignore never assigned to warning for release build.
 #pragma warning disable CS0649
@@ -96,18 +89,12 @@ namespace SsmsSchemaFolders
                  * is from SqlPackageBase.dll and not from Microsoft.SqlServer.SqlTools.VSIntegration.dll
                  * the code below just throws null exception if you have wrong reference */
 
-                _objExplorerService = (ObjectExplorerService)this.GetService(typeof(IObjectExplorerService));
-                foreach (var c in _objExplorerService.Container.Components)
-                {
-                    if (c is ContextService)
-                    {
-                        _cs = (ContextService)c;
-                        _cs.ObjectExplorerContext.CurrentContextChanged += new NodesChangedEventHandler(ObjectExplorerContext_CurrentContextChanged);
-                        break;
-                    }
-                }
-                if (_cs == null)
-                    debug_message("Failed to find ContextService");
+                _objExplorerService = (IObjectExplorerService)this.GetService(typeof(IObjectExplorerService));
+
+                _objectExplorerExtender = new ObjectExplorerExtender(Options);
+
+                AttachTreeViewEvents();
+
             }
             catch (Exception ex)
             {
@@ -145,62 +132,11 @@ namespace SsmsSchemaFolders
 
         #endregion
 
-
-        /// <summary>
-        /// when the object explorer is loaded and change
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        void ObjectExplorerContext_CurrentContextChanged(object sender, NodesChangedEventArgs args)
+        void AttachTreeViewEvents()
         {
-            if (_attached) return;
-            try
-            {
-                if (_trv == null)
-                {
-                    _trv = GetObjectExplorerTreeView();
-                }
-
-                if (_trv != null && !_trv.InvokeRequired)
-                {
-                    if (_objectExplorerExtender == null)
-                    {
-                        debug_message("ObjectExplorerExtender");
-                        _objectExplorerExtender = new ObjectExplorerExtender(Options);
-                        //logger.LogEnd("ObjectExplrerExtender");
-                        if (_trv != null)
-                        {
-                            //logger.LogStart("ImageList");
-                            //if (!_trv.ImageList.Images.ContainsKey("FolderSelected"))
-                            {
-                                //_trv.ImageList.Images.Add("FolderSelected", Resources.folder);
-                                //_trv.ImageList.Images.Add("Table", Resources.tab_ico);
-                                //_trv.ImageList.Images.Add("FolderDown", Resources.folder_closed);
-                                //_trv.ImageList.Images.Add("FolderEdit", Resources.folder);
-                                //_trv.ImageList.Images.Add("FunctionFun", Resources.fun_ico);
-
-                                _trv.BeforeExpand += new TreeViewCancelEventHandler(_trv_BeforeExpand);
-                                _trv.AfterExpand += new TreeViewEventHandler(_trv_AfterExpand);
-                                //TODO: _trv.NodeMouseClick += new TreeNodeMouseClickEventHandler(_trv_NodeMouseClick);
-                                _attached = true;
-                            }
-                            //logger.LogEnd("ImageList");
-                        }
-                    }
-                }
-                else if (_trv != null)
-                {
-                    debug_message(" Inovke Required. sender is " + (sender == null ? "null" : "not null") + ", args is " + (args == null ? "null" : "not null"));
-                    //IntPtr ptr = _trv.Handle;
-                    _trv.BeginInvoke(new NodesChangedEventHandler(ObjectExplorerContext_CurrentContextChanged), new object[] { sender, args });
-                    //-logger.LogEnd("Invoke Required ");
-                }
-            }
-            catch (Exception ObjectExplorerContextException)
-            {
-                debug_message(String.Format("ObjectExplorerContext_CurrentContextChanged::ERROR:{0}", ObjectExplorerContextException.Message));
-            }
-            // */
+            var treeView = GetObjectExplorerTreeView();
+            treeView.BeforeExpand += new TreeViewCancelEventHandler(ObjectExplorerTreeViewBeforeExpandCallback);
+            treeView.AfterExpand += new TreeViewEventHandler(ObjectExplorerTreeViewAfterExpandCallback);
         }
 
         /// <summary>
@@ -209,22 +145,12 @@ namespace SsmsSchemaFolders
         /// <returns></returns>
         private TreeView GetObjectExplorerTreeView()
         {
-
             Type t = _objExplorerService.GetType();
-
-            //FieldInfo field = t.GetField("Tree", BindingFlags.NonPublic | BindingFlags.Instance);
-            FieldInfo field = t.GetField("Tree", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
-
-            if (field != null)
-            {
-                return (TreeView)field.GetValue(_objExplorerService);
-            }
+            PropertyInfo pi = t.GetProperty("Tree", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            if (pi != null)
+                return (TreeView)pi.GetValue(_objExplorerService, null);
             else
-            {
-                PropertyInfo pi = t.GetProperty("Tree", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
-                if (pi != null) return (TreeView)pi.GetValue(_objExplorerService, null);
                 return null;
-            }
         }
 
         /// <summary>
@@ -233,7 +159,7 @@ namespace SsmsSchemaFolders
         /// <param name="node"></param>
         void ReorganizeFolders(TreeNode node)
         {
-            debug_message(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            debug_message("ReorganizeFolders");
             if (!Options.Enabled)
                 return;
             try
@@ -266,10 +192,7 @@ namespace SsmsSchemaFolders
             }
             catch (Exception ex)
             {
-                if (System.Diagnostics.Debugger.IsAttached)
-                    MessageBox.Show(ex.ToString());
-                else
-                    MessageBox.Show(ex.ToString());
+                debug_message(ex.ToString());
             }
         }
 
@@ -279,30 +202,26 @@ namespace SsmsSchemaFolders
         /// </summary>
         /// <param name="sender">object explorer</param>
         /// <param name="e">expanding node</param>
-        void _trv_AfterExpand(object sender, TreeViewEventArgs e)
+        void ObjectExplorerTreeViewAfterExpandCallback(object sender, TreeViewEventArgs e)
         {
-            debug_message(System.Reflection.MethodBase.GetCurrentMethod().Name);
-            // Wait for the async node expand to finish or we could miss indexes
+            debug_message("ObjectExplorerTreeViewAfterExpandCallback");
+            // Wait for the async node expand to finish or we could miss nodes
             try
             {
-                HierarchyTreeNode htn = null;
-                int cnt = 0;
-                while ((htn = e.Node as HierarchyTreeNode) != null && htn.Expanding && cnt < 50000)
+                var node = e.Node as ILazyLoadingNode;
+                int waitCount = 0;
+                while (node != null && node.Expanding && waitCount < 50000)
                 {
                     Application.DoEvents();
-                    cnt++;
+                    waitCount++;
                 }
-                if (_trv.InvokeRequired)
-                    _trv.BeginInvoke(new TrvEventAfterExpand(_trv_AfterExpand), new object[] { sender, e });
-                else
-                    ReorganizeFolders(e.Node);
+                debug_message(String.Format("node.Expanding  waitCount:{0}", waitCount));
+
+                ReorganizeFolders(e.Node);
             }
             catch (Exception ex)
             {
-                if (System.Diagnostics.Debugger.IsAttached)
-                    System.Diagnostics.Debugger.Break();
                 debug_message(ex.ToString());
-                debug_message(ex.StackTrace.ToString());
             }
         }
 
@@ -311,21 +230,16 @@ namespace SsmsSchemaFolders
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void _trv_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        void ObjectExplorerTreeViewBeforeExpandCallback(object sender, TreeViewCancelEventArgs e)
         {
-            debug_message(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            debug_message("ObjectExplorerTreeViewBeforeExpandCallback");
             try
             {
-                if (_trv.InvokeRequired)
-                    _trv.BeginInvoke(new TrvEventBeforeExpand(_trv_BeforeExpand), new object[] { sender, e });
-                else
-                    ReorganizeFolders(e.Node);
+                ReorganizeFolders(e.Node);
             }
             catch (Exception ex)
             {
-                if (System.Diagnostics.Debugger.IsAttached)
-                    System.Diagnostics.Debugger.Break();
-                MessageBox.Show(ex.ToString());
+                debug_message(ex.ToString());
             }
             
         }
@@ -341,7 +255,7 @@ namespace SsmsSchemaFolders
             VsShellUtilities.ShowMessageBox(
                 this,
                 message,
-                "Connect.cs",
+                "Schema Folders",
                 OLEMSGICON.OLEMSGICON_INFO,
                 OLEMSGBUTTON.OLEMSGBUTTON_OK,
                 OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
